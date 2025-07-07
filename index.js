5cc67fcfc8c3b452d33fb08f1e85f293ec3d3d70
Pragya Gaur <gaur273pragya@gmail.com>
2025-07-08 00:14:01 +0530

    updated logic

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 let setup = document.getElementById("setup");
 let punchline = document.getElementById("punchline");
 let question = 0;
+let data;
 
 const getData = async () => {
-  let response = await fetch(
-    "https://official-joke-api.appspot.com/random_joke"
-  );
-  let data = await response.json();
-
-  punchline.style.display = "none";
-
   if (question) {
     punchline.style.display = "block";
     punchline.innerText = data.punchline;
     question = 0;
     btn.innerText = "Next Joke";
   } else {
+    let response = await fetch(
+      "https://official-joke-api.appspot.com/random_joke"
+    );
+    data = await response.json();
+    punchline.style.display = "none";
     setup.innerText = data.setup;
     setup.style.display = "block";
     question = 1;
