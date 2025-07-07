da09bfbb5d466fe6cc80c8c2fcb45907e9c9fe8e
Pragya Gaur <gaur273pragya@gmail.com>
2025-07-07 23:28:33 +0530

    update fetch

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+let setup = document.getElementById("setup");
+let punchline = document.getElementById("punchline");
+
 const getData = async () => {
   let response = await fetch(
     "https://official-joke-api.appspot.com/random_joke"
   );
-  let data = response.json();
-  console.log(data);
+  let data = await response.json();
+  setup.innerText = data.setup;
+  punchline.innerText = data.punchline;
 };
+
+let btn = document.getElementById("generate");
+btn.addEventListener("click", getData);
